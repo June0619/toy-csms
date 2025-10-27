9da9639691c733fe73d7eb1c518083db0346aa44
June0619 <jjw5050@hotmail.com>
2025-10-27 23:33:28 +0900

    feat: initial commit

---
+++ service/csms/src/test/java/me/jwjung/csms/CsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jwjung.csms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
